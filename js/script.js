9d609c2233dd51b62cec12284f46313312ec17e1
yurika <yrpn120293@gmail.com>
2021-06-21 16:24:23 +0900

    [Add]animation

--- js/script.js
+++ js/script.js
@@ -25,3 +25,19 @@ $(".nav__items_sp a").click(function () {
     $(".open__bg").removeClass('__bg-active');
     return false;
 });
+
+/* scroll
+------------------------------------------- */
+$('.nav__item a[href^="#"]').click(function() {
+    var elHash = $(this).attr('href');
+    var pos = $(elHash).offset().top-71;
+    $('body,html').animate({scrollTop: pos}, 500);
+    return false;
+  });
+  
+  $('.nav__item_sp a[href^="#"]').click(function() {
+      var elHash = $(this).attr('href');
+      var smPos = $(elHash).offset().top-51;
+      $('body,html').animate({scrollTop: smPos}, 500);
+      return false;
+  });
